6265bab37deeb59895f438d56fbe8ffab21e6000
Sigr0738 <sigridskovbak@gmail.com>
2026-05-19 21:59:00 +0200

    Tilføjer hentPlaylister og visPlaylister funktioner
    bruger forEach til at loope igennem hver playliste og bygge HTML kort med createElement og appendChild

--- js/script.js
+++ js/script.js
@@ -2,13 +2,33 @@
 // FETCH PLAYLISTER FRA JSON FIL
 // ----------------------------------------
 
-// "async" betyder at funktionen må vente på svar fra fetch. Uden den ville JS fortsætte videre uden at vente på data.
+// "async" skriver jeg foran funktionen fordi den henter data udefra.
+// Det betyder: "vent venligst på svar før du går videre". Uden async/await ville JS ikke vente — og man ville få en tom side.
 async function hentPlaylister() { 
 
-const response = await fetch("data/playlister.json"); //"fetch()" henter data fra min json fil. 
+const response = await fetch("data/playlister.json"); //"fetch()" henter data fra min json fil."await" venter på dataen.
 
-// ".json()" konverterer svaret fra JSON-format til et JS array. Jeg bruger await igen fordi det også tager lidt tid. 
+// ".json()" konverterer svaret (response) fra JSON-format (rå data) til et JS array. Jeg bruger await igen fordi det også tager lidt tid. 
 const playlister = await response.json();
 
-visPlaylister(playlister); // Nu har jeg data - kald funktionen der viser "playlistkortene" på landingpage
+visPlaylister(playlister); // Nu har jeg data som et js array - kald funktionen der viser "playlistkortene" på landingpage
 }
+
+// ----------------------------------------
+// VIS PLAYLISTER I DOM
+// ----------------------------------------
+
+// Funktionen modtager playlister-arrayet som input (parameter)
+// Den looper igennem hver playliste og bygger et HTML kort til hver én.
+function visPlaylister(playlister) {
+
+  // "querySelector()"" finder mit tomme div i HTML med id="playlist-container".
+  const container = document.querySelector("#playlist-container");
+
+  // "forEach" looper igennem hvert objekt ("playliste") i arrayet én af gangen.
+  playlister.forEach((playlist) => {
+
+   
+  });
+}
+
